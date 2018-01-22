cb3334e79c48c3534730b6db18f19ef7fd2c95d8
Edit Bazso <33969923+EditB@users.noreply.github.com>
2018-01-22 22:05:19 +1100

    Version 2
    Added verification for URL format

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ app.use(function(req, res){
   var pathname = url.parse(req.url).pathname;
   //Take the extra slash off
   pathname = pathname.substring(1, pathname.length);
+  first11Digits = pathname.substring(0, 11);
+  console.log(first11Digits);
+  console.log(pathname.length);
+  //Need to make sure the first 11 digits is http://www.; 
+  //also need to make sure the url has the above 11 digits + 4 (for .com) + something else in between;
+  //and it should have a .com in it too.
+  if ((first11Digits == "http://www.") && 
+      (pathname.length > 15) && 
+      (pathname.indexOf(".com") !==-1)) {
+    //We have a valid url format
+    var output = {
+    'original url' : pathname
+    };
+  }
+  else {
+    //not following the needed format:
+    var output = {
+      'error: ' : "Please make sure your url follows the http://www.example.com format"
+    };
+  }
   
   //We need to verify that the url follows the http://www.example.com format; if not, we need to send an error response.
 
@@ -25,12 +45,10 @@ app.use(function(req, res){
   
   //Note: output the pathname with an anchor around it
   
-  var output = {
-    'original url' : pathname
-  };
+  
   
   res.send(output);
-  
+
 })
 
 // listen for requests :)
